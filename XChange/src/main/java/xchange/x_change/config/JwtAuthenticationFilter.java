791676ffaff5b4dc09eddcc53ccee07964db8f1c
oscarsanchez32 <oscsanluq@gmail.com>
2024-05-09 13:45:40 +0200

    login

--- XChange/src/main/java/xchange/x_change/config/JwtAuthenticationFilter.java
+++ XChange/src/main/java/xchange/x_change/config/JwtAuthenticationFilter.java
@@ -1,58 +1,62 @@
 package xchange.x_change.config;
 
-import java.io.IOException;
-
-import org.hibernate.annotations.Comment;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import io.micrometer.common.lang.NonNull;
-import io.micrometer.common.util.StringUtils;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.io.IOException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
-import xchange.x_change.service.UserService;
-import xchange.x_change.util.JwtUtil;
-
+import xchange.x_change.service.UserDetailsServiceImpl;
 
 @Component
-@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    private final JwtUtil jwtUtil;
-    private final UserService userService;
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private JwtUtils jwtUtil;
 
     @Override
-    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain)
-            throws ServletException, IOException {
-        final String authHeader = request.getHeader("Authorization");
-        final String jwt;
-        final String userEmail;
-        if (StringUtils.isEmpty(authHeader) || !StringUtils.startsWith(authHeader, "Bearer ")) {
-            filterChain.doFilter(request, response);
-            return;
-        }
-        jwt = authHeader.substring(7);
-        userEmail = jwtUtil.extractUserName(jwt);
-        if (StringUtils.isNotEmpty(userEmail) && SecurityContextHolder.getContext().getAuthentication()== null){
-            UserDetails userDetails = userService.userDetailsService().loadUserByUsername(userEmail);
-            if (jwtUtil.isTokenValid(jwt, userDetails)){
-                SecurityContext context = SecurityContextHolder.createEmptyContext();
-                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                context.setAuthentication(authToken);
-                SecurityContextHolder.setContext(context);
-                
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String requestTokenHeader = request.getHeader("Authorization");
+        String username = null;
+        String jwtToken = null;
+
+        if(requestTokenHeader != null && requestTokenHeader.startsWith("Bearer ")){
+            jwtToken = requestTokenHeader.substring(7);
+
+            try{
+                username = this.jwtUtil.extractUsername(jwtToken);
+            }catch (ExpiredJwtException exception){
+                System.out.println("El token ha expirado");
+            }catch (Exception e){
+                e.printStackTrace();
             }
+
+        }else{
+            System.out.println("Token invalido , no empieza con bearer string");
         }
-        filterChain.doFilter(request, response);
+
+        if(username != null && SecurityContextHolder.getContext().getAuthentication() == null){
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+            if(this.jwtUtil.validateToken(jwtToken,userDetails)){
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+                usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            }
+        }else{
+            System.out.println("El token no es valido");
+        }
+        filterChain.doFilter(request,response);
     }
 }
--- XChange/src/main/java/xchange/x_change/config/SecurityConfig.java
+++ XChange/src/main/java/xchange/x_change/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
@@ -18,64 +20,111 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import xchange.x_change.security.JwtRequestFilter;
 import xchange.x_change.service.UserDetailsServiceImpl;
-import xchange.x_change.util.JwtUtil;
+import xchange.x_change.config.JwtUtils;
 
 
 @EnableWebSecurity
 @Configuration
-public class SecurityConfig {
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
 
     @Autowired
-    private UserDetailsServiceImpl userDetailsService;
+    private JwtAuthenticationEntryPoint unauthorizedHandler;
 
     @Autowired
-    private JwtUtil jwtUtil;
+    private UserDetailsServiceImpl userDetailsServiceImpl;
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests(authorizeRequests ->
-                authorizeRequests
-                    .antMatchers("/api/public/**").permitAll()
-                    .anyRequest().authenticated())
-            .sessionManagement(sessionManagement ->
-                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .addFilterBefore(new JwtRequestFilter(userDetailsService, jwtUtil), UsernamePasswordAuthenticationFilter.class)
-            .cors().and()
-            .csrf().disable();
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
 
-        return http.build();
-    }
-
-
-    @Bean
-    public WebMvcConfigurer CORSConfigurer(){
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public JwtRequestFilter jwtRequestFilter() {
-    return new JwtRequestFilter(userDetailsService, jwtUtil);
-    }
-
-    @Bean
     @Override
+    @Bean
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(this.userDetailsServiceImpl).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .cors()
+                .disable()
+                .authorizeRequests()
+                .antMatchers("/generate-token","/usuarios/").permitAll()
+                .antMatchers(HttpMethod.OPTIONS).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler)
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
+
+    // @Autowired
+    // private UserDetailsServiceImpl userDetailsService;
+
+    // @Autowired
+    // private JwtUtils jwtUtil;
+
+    // @Bean
+    // public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    //     http
+    //         .authorizeRequests(authorizeRequests ->
+    //             authorizeRequests
+    //                 .antMatchers("/api/public/**").permitAll()
+    //                 .anyRequest().authenticated())
+    //         .sessionManagement(sessionManagement ->
+    //             sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+    //         .addFilterBefore(new JwtRequestFilter(userDetailsService, jwtUtil), UsernamePasswordAuthenticationFilter.class)
+    //         .cors().and()
+    //         .csrf().disable();
+
+    //     return http.build();
+    // }
+
+
+    // @Bean
+    // public WebMvcConfigurer CORSConfigurer(){
+    //     return new WebMvcConfigurer() {
+    //         @Override
+    //         public void addCorsMappings(CorsRegistry registry) {
+    //             registry.addMapping("/**")
+    //                     .allowedOrigins("http://localhost:4200")
+    //                     .allowedMethods("GET", "POST", "PUT", "DELETE")
+    //                     .allowedHeaders("*")
+    //                     .allowCredentials(true);
+    //         }
+    //     };
+    // }
+
+    // @Bean
+    // public PasswordEncoder passwordEncoder() {
+    //     return new BCryptPasswordEncoder();
+    // }
+
+    // @Bean
+    // public JwtRequestFilter jwtRequestFilter() {
+    // return new JwtRequestFilter(userDetailsService, jwtUtil);
+    // }
+
+    // @Bean
+    // @Override
+    // public AuthenticationManager authenticationManagerBean() throws Exception {
+    //     return super.authenticationManagerBean();
+    // }
 
 }
